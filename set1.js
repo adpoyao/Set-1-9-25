e80ff1dc2a614662b9e89e1965bc80235cadea53
Quang Nguyen <qtn2@njit.edu>
2017-09-25 15:20:25 -0400

    problem 6

--- set1.js
+++ set1.js
@@ -1,15 +1,26 @@
 'use strict'
 
 function yearOfBirth(age) {
+  
   return 2017 - age;
   
 }
 
 function whoAmI(name, age) {
   let yob = yearOfBirth(age);
+  if (age < 0) {
+    throw new Error('Age cannnot be negative');
+  }
 
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yob}`)
+  
+    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+    console.log(`I was born in ${yob}`)
 }
 
-whoAmI('Quang', 27);
+try {
+  whoAmI('Quang', -5);
+}
+catch(e) {
+  console.error(e.message);
+}
+
