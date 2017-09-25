62212f0ed39f1e63181f58c9da087d5ec94f078d
Eddie Yao <adpoyao@gmail.com>
2017-09-25 13:00:23 -0700

    Infinity and Beyond codes

--- set2.js
+++ set2.js
@@ -7,3 +7,20 @@ function jediName(firstName, lastName) {
 }
 
 jediName('Beyonce','Eddie');
+
+function beyond(num) {
+  if(num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY) {
+    console.log('and beyond')
+    ;} 
+  else if(Number.isFinite(num) && num > 0) {
+    console.log('To Inifinity')
+    ;}
+  else if(Number.isFinite(num) && num < 0) {
+    console.log('To Negative')
+    ;}
+  else if(num === 0) {
+    console.log('staying home');
+  }
+}
+
+beyond(-0);
