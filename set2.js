563d0503835b0e4b4c50204cfb4cbb061c665264
Andy Gaines <andyagaines@gmail.com>
2017-09-25 16:02:56 -0500

    alternative solution to decoder

--- set2.js
+++ set2.js
@@ -6,7 +6,7 @@ function jediName(firstName, lastName) {
   return partialLast + partialFirst;
 }
 
-jediName('Beyonce','Eddie');
+// jediName('Beyonce','Eddie');
 
 function beyond(num) {
   if(num === Number.POSITIVE_INFINITY || num === Number.NEGATIVE_INFINITY) {
@@ -23,11 +23,20 @@ function beyond(num) {
   }
 }
 
-beyond(-0);
+//beyond(-0);
 
 function decode(sentence) {
-  const arr = sentence.split(" ")
-  const decoded = [];
+  const key = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+  };
+
+  //If first letter is a, b, c, or d, we return nth character of that word.
+
+  const arr = sentence.split(' ')
+  let decoded = '';
 
   for (let i = 0; i < arr.length; i++) {
     let word = arr[i];
@@ -53,7 +62,33 @@ function decode(sentence) {
     }
   }
 
-  return decoded.join("");
+  return decoded.join('');
 }
 
-decode('craft block argon meter bells brown croon droop');
+// decode('craft block argon meter bells brown croon droop');
+
+function monthCount(month) {
+  switch(month) {
+
+  case 'January':
+  case 'March':
+  case 'May':
+  case 'July':
+  case 'August':
+  case 'October':
+  case 'December':
+    console.log(`${month} has 31 days.`);
+    break;
+
+  case 'February':
+    console.log(`${month} has 28 or 29 days.`);
+    break;
+
+  default:
+    console.log(`${month} has 30 days.`);
+    break;
+  }
+
+}
+
+monthCount('August');
