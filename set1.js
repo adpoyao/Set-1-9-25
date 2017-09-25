a7d0702cc3108f876147e11830d3b4e328badf84
Quang Nguyen <qtn2@njit.edu>
2017-09-25 14:57:57 -0400

    problem 4

--- set1.js
+++ set1.js
@@ -1,7 +1,11 @@
 'use strict'
 
+
+
 function whoAmI(name, age) {
+  let yearOfBirth = 2017 - age;
   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yearOfBirth}`)
 }
 
-whoAmI();
+whoAmI('Quang', 27);
